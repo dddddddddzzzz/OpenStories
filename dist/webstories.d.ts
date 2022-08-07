eba77f6e57a32507e7d479ea7d2b58aaf42d5515
Mu-An Chiou <me@muanchiou.com>
2022-08-07 23:31:38 +0800

    Add web story basics

--- dist/webstories.d.ts
+++ dist/webstories.d.ts
@@ -1,11 +1,33 @@
 import JSONFeed from './jsonfeed'
 
 interface WebStoriesFeed extends JSONFeed {
-  _web_story: WebStoryItem
+  _web_story: WebStoryItem;
+  _web_storybackground_color: string;
 }
 
 interface WebStoryItem {
+  story: ImageStory | VideoStory;
+  video?: VideoStory;
+  blur_hash?: string;
+  background_color?: string;
+  senstivity_warning?: string;
 }
 
-export default WebStoriesFeed
+interface WebStory {
+  url: string;
+  mime_type: string;
+}
 
+interface ImageStory extends WebStory {
+  type: 'image';
+  alt: string;
+  caption: string;
+}
+
+interface VideoStory extends WebStory {
+  type: 'video';
+  transcript?: string;
+  duration_in_seconds?: number;
+}
+
+export default WebStoriesFeed;
