263f7f1f6d76f17dd75f801633698b64145494c8
Mu-An Chiou <me@muanchiou.com>
2022-08-07 22:56:31 +0800

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,40 @@
+interface JSONFeed {
+  version: string;
+  title: string;
+  home_page_url?: string;
+  feed_url?: string;
+  description?: string;
+  user_comment?: string;
+  next_url?: string;
+  icon?: string;
+  favicon?: string;
+  authors?: Author[];
+  language?: string;
+  expired?: boolean;
+  hubs?: Hub[];
+  items: FeedItem[];
+}
+
+interface Hub {
+
+}
+
+interface Author {
+  name?: string;
+  url?: string;
+  avatar?: string;
+}
+
+interface FeedItem {
+}
+
+// 
+
+interface WebStoriesFeed extends JSONFeed {
+  _web_story: WebStoryItem
+}
+
+interface WebStoryItem {
+}
+
+export {JSONFeed, WebStoriesFeed}
